ac08bc259aa88fef1ad2be4b80b3674375143ee9
AnyamahNdidi <theo4felix@gmail.com>
2023-04-02 11:06:59 -0700

    little feature added

--- client/frontend/src/component/miscellanea/SideDrawer.js
+++ client/frontend/src/component/miscellanea/SideDrawer.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {Box} from "@chakra-ui/layout"
-import {Tooltip} from "@chakra-ui/react"
+import { Tooltip, Button, Text} from '@chakra-ui/react'
 
 const SideDrawer = () => {
   const [search, setSearch] = useState("")
@@ -11,6 +11,15 @@ const SideDrawer = () => {
   return (
     <>
       <Box>
+        <Tooltip label="Search Users to chat">
+          <Button >
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px={4}>
+              Search User
+            </Text>
+          </Button>
+          
+        </Tooltip>
      
       </Box>
     
